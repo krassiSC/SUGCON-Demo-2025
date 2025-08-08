a7e4286f9ecd06c8ad51010cd2d88f9402ee206a
Joseph Long <joseph.long@sitecore.com>
2025-08-08 16:57:15 +0100

    Manually added component.

--- examples/kit-nextjs-location-finder/.sitecore/component-map.ts
+++ examples/kit-nextjs-location-finder/.sitecore/component-map.ts
@@ -7,6 +7,7 @@ import { Form } from '@sitecore-content-sdk/nextjs';
 import * as AccordionBlock from 'components/accordion-block/AccordionBlock';
 import * as AlertBanner from 'components/alert-banner/AlertBanner.dev';
 import * as ArticleHeader from 'components/article-header/ArticleHeader';
+import * as ArticleContentHub from 'components/ArticleContentHub';
 import * as BackgroundThumbnail from 'components/background-thumbnail/BackgroundThumbnail.dev';
 import * as Breadcrumbs from 'components/breadcrumbs/Breadcrumbs';
 import * as ButtonComponent from 'components/button-component/ButtonComponent';
@@ -95,6 +96,7 @@ import * as ZipcodeModal from 'components/zipcode-modal/zipcode-modal.dev';
 export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['AccordionBlock', AccordionBlock],
   ['AlertBanner', AlertBanner],
+  ['ArticleContentHub', ArticleContentHub],
   ['ArticleHeader', ArticleHeader],
   ['BackgroundThumbnail', BackgroundThumbnail],
   ['Breadcrumbs', Breadcrumbs],
